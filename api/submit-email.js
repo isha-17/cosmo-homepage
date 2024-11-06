b3b108c8b213aaf935fc9e0fb27b783623616db6
Isha Gupta <isha.gupta@positrigo.com>
2024-11-06 11:37:19 +0100

    seperate js?

---
+++ api/submit-email.js
@@ -0,0 +1,33 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: process.env.GOOGLE_API_KEY,
+    authDomain: "reccapp-26231.firebaseapp.com",
+    databaseURL: process.env.GOOGLE_DATABASE_URL,
+    projectId: "reccapp-26231",
+    storageBucket: "reccapp-26231.appspot.com",
+    messagingSenderId: "261665606195",
+    appId: process.env.GOOGLE_APP_ID,
+    measurementId: "G-YZ6V9R6G5J"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default async (req, res) => {
+    if (req.method === "POST") {
+        const { email } = req.body;
+        const signupDate = new Date().toISOString();
+
+        try {
+            await setDoc(doc(db, "emails", email), { signup: signupDate });
+            res.status(200).json({ message: "Email submitted successfully!" });
+        } catch (error) {
+            console.error("Error adding email: ", error);
+            res.status(500).json({ error: "Failed to submit email." });
+        }
+    } else {
+        res.status(405).json({ error: "Method not allowed" });
+    }
+};
